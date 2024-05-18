146b63246f6f86080b6f1885809db9fea30445c8
jpaliaga10 <155824170+jpaliaga10@users.noreply.github.com>
2024-05-18 17:20:04 +0000

    ahora si

--- src/models.py
+++ src/models.py
@@ -1,47 +1,49 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-from sqlalchemy import Enum
+
 Base = declarative_base()
 
 class User(Base):
-    __tablename__='user'
+    __tablename__ = 'user'
     Id = Column(Integer, primary_key=True)
-    Username = Column(String(20), nullable = False, unique = True)
-    Email = Column(String(20), nullable = False, unique = True)
+    Username = Column(String(20), nullable=False, unique=True)
+    Email = Column(String(20), nullable=False, unique=True)
 
 class Follower(Base):
-    __tablename__='follower'
+    __tablename__ = 'follower'
     Id = Column(Integer, primary_key=True)
     UserFromId = Column(Integer, ForeignKey('user.Id'))
     UserToId = Column(Integer, ForeignKey('user.Id'))
-    user = relationship(User)
+    user_from = relationship(User)
+    user_to = relationship(User)
 
 class Post(Base):
-    __tablename__='post'
+    __tablename__ = 'post'
     Id = Column(Integer, primary_key=True)
     UserFromId = Column(Integer, ForeignKey('user.Id'))
     UserToId = Column(Integer, ForeignKey('user.Id'))
-    user = relationship(User)
+    UserFromId = relationship(User)
 
 class Media(Base):
-    __tablename__='media'
+    __tablename__ = 'media'
     Id = Column(Integer, primary_key=True)
-    Type = Column(Enum('admin', 'user', name='user_type'))
+    Type = Column(Enum('admin', 'user', name='media_type'))
     Url = Column(String(50), nullable=True)
     PostId = Column(Integer, ForeignKey('post.Id'))
-    UserToId = Column(Integer, ForeignKey('post.Id'))
     post = relationship(Post)
 
 class Comment(Base):
-    __tablename__='comment'
+    __tablename__ = 'comment'
     Id = Column(Integer, primary_key=True)
     comment_text = Column(String(30), nullable=True)
     author_id = Column(Integer, ForeignKey('user.Id'))
     post_id = Column(Integer, ForeignKey('post.Id'))
+    author = relationship(User)
+    post = relationship(Post)
 
     def to_dict(self):
         return {}
@@ -51,5 +53,5 @@ try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
